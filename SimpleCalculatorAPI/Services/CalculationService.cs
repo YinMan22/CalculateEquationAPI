9e5d45250bf181cde37c1e46fdadcb6ca8259857
yw <mukym-wm19@student.tarc.edu.my>
2024-09-19 13:48:30 +0800

    touch up

--- SimpleCalculatorAPI/Services/CalculationService.cs
+++ SimpleCalculatorAPI/Services/CalculationService.cs
@@ -8,6 +8,7 @@ namespace SimpleCalculatorAPI.Services
     public class CalculationService : ICalculationService
     {
         private readonly ILogger<CalculationService> _logger;
+        private const string regEx = @"^[0-9+\-*/().\s]+$";
 
         public CalculationService(ILogger<CalculationService> logger)
         {
@@ -18,14 +19,13 @@ namespace SimpleCalculatorAPI.Services
         {
             try 
             {
-                _logger.LogInformation($"CalculateMathOperation Request received: {operation}");
+                _logger.LogInformation($"{nameof(CalculateMathOperation)} | Request received: {operation}");
 
-                // validate number and operator only
-                string regEx = @"^[0-9+\-*/().\s]+$";
-
+                // Validate number and operator only
                 if (!Regex.IsMatch(operation, regEx))
                     throw new SyntaxErrorException("Invalid characters exist.");
 
+                // Evaluating math operation
                 DataTable dataTable = new DataTable();
                 double result = Convert.ToDouble(dataTable.Compute(operation, ""));
 
@@ -33,17 +33,17 @@ namespace SimpleCalculatorAPI.Services
                 if (double.IsInfinity(result) || double.IsNaN(result))
                     throw new SyntaxErrorException("Invalid mathematical operation.");
 
-                _logger.LogInformation($"CalculateMathOperation Response returned: {result}");
+                _logger.LogInformation($"{nameof(CalculateMathOperation)} | Response returned: {result}");
                 return result;
             }
             catch (SyntaxErrorException ex)
             {
-                _logger.LogError($"CalculateMathOperation SyntaxError occurred: {ex.Message} | Request received: {operation}");
+                _logger.LogError($"{nameof(CalculateMathOperation)} | SyntaxError occurred: {ex.Message} | Request received: {operation}");
                 throw new ArgumentException($"{ex.Message}");
             }
             catch (Exception ex)
             {
-                _logger.LogError($"CalculateMathOperation Error occurred: {ex.Message} | Request received: {operation}");
+                _logger.LogError($"{nameof(CalculateMathOperation)} | Error occurred: {ex.Message} | Request received: {operation}");
                 throw new Exception($"Error when evaluating the mathematical operation.");
             }
         }
